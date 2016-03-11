5d0ac9410d9ffce3785da57906dde44bfbd2d70f
sparanoid <t@sparanoid.com>
2016-03-11 13:33:12 +0800

    feat: init release

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const header = require('gulp-header');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const pkg = require('./package.json');
+
+const src = 'lightense.es6';
+const dest = './';
+const banner = '/*! <%= pkg.name %> v<%= pkg.version %> | (c) <%= pkg.author %> | <%= pkg.license %> */\n';
+
+gulp.task('default', () =>
+  gulp.src(src)
+    .pipe(babel({ babelrc: true }))
+    .pipe(gulp.dest(dest))
+    .pipe(uglify())
+    .pipe(header(banner, { pkg : pkg }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(dest))
+);
