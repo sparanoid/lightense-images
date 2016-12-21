da959b9344fdbdd3ae53846967f5d04776401a87
sparanoid <t@sparanoid.com>
2016-12-21 14:52:31 +0800

    chore: release 1.0.2

--- lightense.js
+++ lightense.js
@@ -1,4 +1,4 @@
-/*! lightense-images v1.0.1 | © Sparanoid | MIT */
+/*! lightense-images v1.0.2 | © Sparanoid | MIT */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
