eb0f1e8e4f617206c4b89305ae37f557bdf3e8fc
Tunghsiao Liu <t@sparanoid.com>
2018-12-09 23:55:53 +0800

    chore: release 1.0.6

--- lightense.js
+++ lightense.js
@@ -1,4 +1,4 @@
-/*! lightense-images v1.0.5 | © Tunghsiao Liu | MIT */
+/*! lightense-images v1.0.6 | © Tunghsiao Liu | MIT */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
