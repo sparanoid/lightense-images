e89edf93f53c78d366cc06f70d845bd4ac3f7b07
Tunghsiao Liu <t@sparanoid.com>
2019-02-20 18:24:20 +0800

    v1.0.8

--- lightense.js
+++ lightense.js
@@ -1,4 +1,4 @@
-/*! lightense-images v1.0.7 | © Tunghsiao Liu | MIT */
+/*! lightense-images v1.0.8 | © Tunghsiao Liu | MIT */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
