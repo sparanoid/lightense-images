389c03ff721a4733e90e1918bc9e5b1d95032983
sparanoid <t@sparanoid.com>
2019-09-01 21:45:56 +0800

    fix(js): z-index too high, ref https://github.com/sparanoid/lightense-images/issues/41

--- lightense.js
+++ lightense.js
@@ -118,7 +118,7 @@ var Lightense = function Lightense() {
     keyboard: true,
     cubicBezier: 'cubic-bezier(.2, 0, .1, 1)',
     background: 'rgba(255, 255, 255, .98)',
-    zIndex: 2147483647,
+    zIndex: 1000000,
 
     /* eslint-disable no-undefined */
     beforeShow: undefined,
