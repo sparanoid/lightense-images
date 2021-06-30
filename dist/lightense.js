5748a2a617faea261611fe6b3f6fc30ab23346d0
sparanoid <t@sparanoid.com>
2021-06-30 10:43:55 -0400

    Release 1.0.12

--- dist/lightense.js
+++ dist/lightense.js
@@ -1,4 +1,4 @@
-/*! lightense-images v1.0.11 | © Sparanoid | MIT */
+/*! lightense-images v1.0.12 | © Sparanoid | MIT */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
